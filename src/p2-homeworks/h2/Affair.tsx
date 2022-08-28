69fa2f7f469bd688bef92331016e1ebaab0c143e
Denis <109683124+DenisBabeny@users.noreply.github.com>
2022-08-28 12:38:40 +0300

    update

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -2,23 +2,21 @@ import React from 'react'
 import {AffairType} from "./HW2";
 
 
-
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType // need to fix any
-    deleteAffairCallback: (id:number) => void // need to fix any
+    deleteAffairCallback: (id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
- return props.deleteAffairCallback(props.affair._id)
+        return props.deleteAffairCallback(props.affair._id)
     }
     // need to fix
 
     return (
         <div>
-            // show some text
-
+            {props.affair.name}
             <button onClick={deleteCallback}>X</button>
         </div>
     )
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -4,9 +4,9 @@ import {AffairType, FilterType} from './HW2'
 
 
 type AffairsPropsType = { // need to fix any
-    data:AffairType[];
+    data: AffairType[];
     setFilter: (filter: FilterType) => void;
-    deleteAffairCallback:(id:number)=> void;
+    deleteAffairCallback: (id: number) => void;
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -19,13 +19,16 @@ function Affairs(props: AffairsPropsType) {
     ))
 
     const setAll = () => {
-
+        return props.setFilter('all')
     } // need to fix
     const setHigh = () => {
+        props.setFilter('high')
     }
     const setMiddle = () => {
+        props.setFilter('middle')
     }
     const setLow = () => {
+        props.setFilter('low')
     }
 
     return (
